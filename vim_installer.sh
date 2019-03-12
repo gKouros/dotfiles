32ec4d0823d9fab8a2357d3689b39d44e67cbe7c
George Kouros <george@kapernikov.com>
2019-03-12 14:06:17 +0100

    Fix vim installer youcompleteme installation

--- vim_installer.sh
+++ vim_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install vim and plugins
-sudo apt-get -y install vim exuberant-ctags
+sudo apt-get -y install vim exuberant-ctags build-essential cmake python3-dev
 ln -s $(pwd)/.vimrc ~/.vim/vimrc
 sudo ln -fs ~/.vim/vimrc /etc/vim/vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -9,4 +9,4 @@ git clone https://github.com/vim-scripts/python.vim.git ~/.vim/python.vim
 git clone https://github.com/powerline/fonts.git ~/.vim/fonts
 vim +PluginInstall +qall
 ~/.vim/fonts/install.sh
-~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer
