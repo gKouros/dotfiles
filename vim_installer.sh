6ae98a5af3e10bdbdba09bb00fdea6c87834f91e
George Kouros <george.kouros.ece@gmail.com>
2020-03-21 18:37:20 +0100

    Update vim installer

--- vim_installer.sh
+++ vim_installer.sh
@@ -3,13 +3,13 @@
 DPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # install vim and plugins
-sudo apt-get -y install vim exuberant-ctags build-essential cmake python{,3}-dev g++ grip xdotool
+sudo apt-get -y install vim exuberant-ctags build-essential cmake python-dev python3-dev g++ grip xdotool
 ln -fs $DPATH/.vimrc ~/.vim/vimrc
 sudo ln -fs ~/.vim/vimrc /etc/vim/vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/vim-scripts/python.vim.git ~/.vim/python.vim
 git clone https://github.com/powerline/fonts.git ~/.vim/fonts
-vim +PluginInstall +qall
 ~/.vim/fonts/install.sh
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+vim +PluginInstall +qall
