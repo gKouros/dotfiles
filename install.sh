ad4c7672d03c45c9c558f4e14f92594a72cb3d6c
George Kouros <george.kouros.ece@gmail.com>
2020-03-29 10:56:19 +0200

    Update install script and programs

--- install.sh
+++ install.sh
@@ -6,20 +6,9 @@ dpath=$(pwd)
 ln -fs $dpath/.bashrc ~/.bashrc
 ln -fs $dpath/.aliases ~/.aliases
 ln -fs $dpath/.bash_scripts ~/.bash_scripts
-sudo apt-get install git && ln -fs $dpath/.gitconfig ~/.gitconfig
+sudo pacman -S --noconfirm git
+ln -fs $dpath/.gitconfig ~/.gitconfig
+ln -fs $dpath/.gitignore_global ~/.gitignore_global
 
 # install zsh
 sh $dpath/zsh_installer.sh
-
-# install vim and plugins
-sh $dpath/vim_installer.sh
-
-# install programs from file
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-for program in `cat programs_list.txt`
-do
-	echo "\033[0;32mInstalling $program\033[0m"
-	sudo apt-get install $program -y
-done
