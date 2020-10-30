6629d2eee02a8da9c5761fefc4633c591f872330
George Kouros <george.kouros.ece@gmail.com>
2020-10-30 20:35:09 +0100

    Fix neovim installer for ubuntu

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm neovim python-neovim python{,2} xdotool uncrustify \
-pip2 install --user neovim pynvim
-pip install --user neovim pynvim grip jedi \
+sudo apt install -y neovim python-neovim python3 python3-pip xdotool uncrustify curl
+#pip2 install --user neovim pynvim
+pip3 install --user neovim pynvim grip jedi \
     yapf cmake-format remark pyaml flake8 pylint
-pip install --user 'msgpack>=1.0.0'
+pip3 install --user 'msgpack>=1.0.0'
 mkdir -p $HOME/.config/nvim
 ln -s $(pwd)/init.vim $HOME/.config/nvim/init.vim
 
@@ -16,8 +16,8 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install plugins
-vim +PlugInstall +:q +:q
+vim +PlugInstall +:q
 
 # install language servers
-https://github.com/cquery-project/cquery
-https://github.com/palantir/python-language-server
+# https://github.com/cquery-project/cquery
+# https://github.com/palantir/python-language-server
