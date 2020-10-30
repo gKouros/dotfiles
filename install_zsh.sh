cc1db4c6fb59ac6afd4fcebd6ccfcd2d462837c1
George Kouros <george.kouros.ece@gmail.com>
2020-10-30 20:35:43 +0100

    Fix zsh installation

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 ZSH=~/.oh-my-zsh
 ZSH_CUSTOM=~/.oh-my-zsh/custom
-DPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+dpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # install zsh
-sudo apt install zsh -y
+sudo apt install -y zsh wget
 
 # Install oh_my_zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | zsh || true
 
 # Install zsh_autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -19,5 +18,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 # Install zsh_history_substring_search
 git clone https://github.com/zsh-users/zsh-history-substring-search.git $ZSH_CUSTOM/plugins/zsh-history-substring-search
 
-ln -sf $DPATH/.zshrc ~/.zshrc
-ln -sf $DPATH/my.zsh-theme $ZSH/themes/my_zsh_theme.zsh-theme
+ln -sf $dpath/.zshrc ~/.zshrc
+ln -sf $dpath/my.zsh-theme $ZSH/themes/my_zsh_theme.zsh-theme
+sudo ln -sf $HOME/.oh-my-zsh /root/.oh-my-zsh
+sudo ln -sf $HOME/.zshrc /root/.zshrc
+
+sudo chsh -s /bin/zsh $USER
